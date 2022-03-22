6712754ec9bc2855123ea8de967ea3dd36ad42ae
Kumpis Audrius <audrius.kumpis@mif.stud.vu.lt>
2022-03-22 20:41:25 +0200

    OPD-2 | fix failing test

--- src/test/java/com/oopsiedaisy/ApplicationTests.java
+++ src/test/java/com/oopsiedaisy/ApplicationTests.java
@@ -3,18 +3,11 @@ package com.oopsiedaisy;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-
 @SpringBootTest
 class ApplicationTests {
 
 	@Test
 	void contextLoads() {
-	}
-
-	@Test
-	void shouldFail() {
-		assertThat(5).isEqualTo(5);
 	}
 
 }
