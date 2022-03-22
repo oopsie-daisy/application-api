97b163f39ba6cd091108cdfeb27a892cb387b830
Kumpis Audrius <audrius.kumpis@mif.stud.vu.lt>
2022-03-22 19:38:34 +0200

    base files

---
+++ src/main/java/com/oopsiedaisy/OopsiedaisyApplication.java
@@ -0,0 +1,13 @@
+package com.oopsiedaisy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopsiedaisyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OopsiedaisyApplication.class, args);
+	}
+
+}
