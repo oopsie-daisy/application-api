6a97821e85db687bef9c672445ed70a13e656d16
Kumpis Audrius <audrius.kumpis@mif.stud.vu.lt>
2022-03-22 20:32:25 +0200

    OPD-2 | test ci cd pipeline

--- src/test/java/com/oopsiedaisy/ApplicationTests.java
+++ src/test/java/com/oopsiedaisy/ApplicationTests.java
@@ -3,11 +3,18 @@ package com.oopsiedaisy;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 @SpringBootTest
 class ApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void shouldFail() {
+		assertThat(5).isEqualTo(4);
 	}
 
 }
