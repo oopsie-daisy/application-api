ef0b4ea4dd7ca8fe8341ee7b3d6e3eb957625e83
Kumpis Audrius <audrius.kumpis@mif.stud.vu.lt>
2022-05-15 17:16:51 +0300

    OPD-39 | code cleanup

--- src/main/java/com/oopsiedaisy/payments/service/PaymentService.java
+++ src/main/java/com/oopsiedaisy/payments/service/PaymentService.java
@@ -14,7 +14,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.util.List;
-import java.util.UUID;
 
 import static com.oopsiedaisy.payments.controller.resource.PaymentStatus.COMPLETED;
 import static com.oopsiedaisy.payments.controller.resource.PaymentStatus.FAILED;
@@ -32,11 +31,12 @@ public class PaymentService {
     private final PaymentRepository paymentRepository;
 
     public PaymentStatus completePayment(ItemsToBuy itemsToBuy) {
-        List<Flower> flowersToBeSold = repository.getByUuids(itemsToBuy.getItems());
-        if (flowersToBeSold.size() != itemsToBuy.getItems().size()) {
-            savePaymentInfo(itemsToBuy.getPaymentProvider(), itemsToBuy.getAmountToPay(), itemsToBuy.getSenderIban(), FAILED);
-            throw new FailedPaymentException(NO_ITEMS_AVAILABLE);
-        }
+        checkIfItemsExist(itemsToBuy);
+        doPaymentInternal(itemsToBuy);
+        return COMPLETED;
+    }
+
+    private void doPaymentInternal(ItemsToBuy itemsToBuy) {
         try {
             PaymentStatus status = repository.removeAll(itemsToBuy.getItems());
             savePaymentInfo(itemsToBuy.getPaymentProvider(), itemsToBuy.getAmountToPay(), itemsToBuy.getSenderIban(), status);
@@ -44,7 +44,14 @@ public class PaymentService {
             savePaymentInfo(itemsToBuy.getPaymentProvider(), itemsToBuy.getAmountToPay(), itemsToBuy.getSenderIban(), FAILED);
             throw e;
         }
-        return COMPLETED;
+    }
+
+    private void checkIfItemsExist(ItemsToBuy itemsToBuy) {
+        List<Flower> flowersToBeSold = repository.getByUuids(itemsToBuy.getItems());
+        if (flowersToBeSold.size() != itemsToBuy.getItems().size()) {
+            savePaymentInfo(itemsToBuy.getPaymentProvider(), itemsToBuy.getAmountToPay(), itemsToBuy.getSenderIban(), FAILED);
+            throw new FailedPaymentException(NO_ITEMS_AVAILABLE);
+        }
     }
 
     private void savePaymentInfo(PaymentProvider paymentProvider, BigDecimal amountToPay, String senderIban, PaymentStatus status) {
